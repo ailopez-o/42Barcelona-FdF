65faa7571e37eb8ba932e0942343b729490bad70
ailopez- <ailopez-@student.42barcelona.com>
2022-06-29 11:37:39 +0200

    norm

--- src/utils.c
+++ src/utils.c
@@ -95,13 +95,10 @@ int gradient(int startcolor, int endcolor, int len, int pix)
     increment[0] = (double)((R(endcolor)) - (R(startcolor))) / (double)len;
     increment[1] = (double)((G(endcolor)) - (G(startcolor))) / (double)len;
     increment[2] = (double)((B(endcolor)) - (B(startcolor))) / (double)len;
-
     new[0] = (R(startcolor)) + ft_round(pix * increment[0]);
     new[1] = (G(startcolor)) + ft_round(pix * increment[1]);
     new[2] = (B(startcolor)) + ft_round(pix * increment[2]);
-
     newcolor = RGB(new[0], new[1], new[2]);
-
     return (newcolor);
 }
 
@@ -180,7 +177,6 @@ void generate_background(t_meta *meta, int color)
         }
         Y++;
         X = 0;
-        //if (y%3 == 0) color++;
     }
 }
 
