52e8630da40a0c8f7c41cecd72d5d24adfc2279f
Aitor Lopez de audicana oar <ailopez-@cbr14s4.42barcelona.com>
2022-07-06 00:12:32 +0200

    norm

--- src/geometry.c
+++ src/geometry.c
@@ -6,92 +6,96 @@
 /*   By: ailopez- <ailopez-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 16:01:10 by ailopez-          #+#    #+#             */
-/*   Updated: 2022/06/29 16:01:17 by ailopez-         ###   ########.fr       */
+/*   Updated: 2022/07/06 00:12:08 by ailopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/fdf.h"
 
-
-void traslate(t_point *points, t_point move, int len)
+void	traslate(t_point *points, t_point move, int len)
 {
-    int     i;
-    i = 0;
-    while (i < len)
-    {
-        points[i].axis[x] = points[i].axis[x] + move.axis[x];
-        points[i].axis[y] = points[i].axis[y] + move.axis[y];
-        points[i].axis[z] = points[i].axis[z] + move.axis[z];
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		points[i].axis[x] = points[i].axis[x] + move.axis[x];
+		points[i].axis[y] = points[i].axis[y] + move.axis[y];
+		points[i].axis[z] = points[i].axis[z] + move.axis[z];
+		i++;
+	}
 }
 
-void center(t_point *points, int len)
+void	center(t_point *points, int len)
 {
-    int     i;
-    i = 0;
-    while (i < len)
-    {
-        points[i].axis[x] = points[i].axis[x] + WINX/2;
-        points[i].axis[y] = points[i].axis[y] + WINY/2;
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		points[i].axis[x] = points[i].axis[x] + WINX / 2;
+		points[i].axis[y] = points[i].axis[y] + WINY / 2;
+		i++;
+	}
 }
 
-int limits(t_point  *points, int len)
+int	limits(t_point *points, int len)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < len)
-    {
-        if (points[i].axis[x] < FIT_MARGIN || points[i].axis[x] > (WINX - FIT_MARGIN))
-            return (1);
-        if (points[i].axis[y] < FIT_MARGIN || points[i].axis[y] > (WINY - FIT_MARGIN))
-            return (1);
-        i++;    
-    }
-    return(0);
+	i = 0;
+	while (i < len)
+	{
+		if (points[i].axis[x] < FIT_MARGIN || \
+			points[i].axis[x] > (WINX - FIT_MARGIN))
+			return (1);
+		if (points[i].axis[y] < FIT_MARGIN || \
+			points[i].axis[y] > (WINY - FIT_MARGIN))
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-void scale(t_point *points, int scale, int len)
+void	scale(t_point *points, int scale, int len)
 {
-    int     i;
-    i = 0;
-    while (i < len)
-    {
-        points[i].axis[x] = points[i].axis[x] * scale;
-        points[i].axis[y] = points[i].axis[y] * scale;
-        points[i].axis[z] = points[i].axis[z] * scale;
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (i < len)
+	{
+		points[i].axis[x] = points[i].axis[x] * scale;
+		points[i].axis[y] = points[i].axis[y] * scale;
+		points[i].axis[z] = points[i].axis[z] * scale;
+		i++;
+	}
 }
 
-int fit(t_map *map)
+int	fit(t_map *map)
 {
 	t_point		*proyect;
 
 	proyect = malloc (map->len * sizeof(t_point));
 	if (proyect == NULL)
 		return (-1);
-    rotate_y(proyect, proyect, map->ang[y], map->len);
-	rotate_z(proyect, proyect, map->ang[z], map->len);	
+	rotate_y(proyect, proyect, map->ang[y], map->len);
+	rotate_z(proyect, proyect, map->ang[z], map->len);
 	orto_proyection (proyect, proyect, map->len);
 	scale (proyect, map->scale, map->len);
-	traslate(proyect, map->source, map->len);	
-    while(!limits(proyect, map->len))
-    {
-        map->scale++;
-        scale(proyect, map->scale, map->len);
-    }
-    free (proyect);
-    return(1);
+	traslate(proyect, map->source, map->len);
+	while (!limits(proyect, map->len))
+	{
+		map->scale++;
+		scale(proyect, map->scale, map->len);
+	}
+	free (proyect);
+	return (1);
 }
 
-void isometric(t_map *map)
+void	isometric(t_map *map)
 {
-    map->ang[x] = 30;
-    map->ang[y] = 330;
-    map->ang[z] = 30;    
-    map->source.axis[x] = WINX/2;
-    map->source.axis[y] = WINY/2;
+	map->ang[x] = 30;
+	map->ang[y] = 330;
+	map->ang[z] = 30;
+	map->source.axis[x] = WINX / 2;
+	map->source.axis[y] = WINY / 2;
 }
--- src/mouse.c
+++ src/mouse.c
@@ -16,30 +16,26 @@ int	mouse_move(int X, int Y, void *param)
 	t_meta	*meta;
 
 	meta = (t_meta *)param;
-
 	if (X > WINX || X < 0 || Y > WINY || Y < 0)
-		return(0);
-	//printf("Mouse Press [%d][%d]\n", X,Y);
+		return (0);
 	if (meta->keys.b_mouseL)
 	{
-		//printf("LastClickL [%d][%d] - Currect Click [%d][%d] - Increment [%d][%d]\n", (int)meta->last_clickL.axis[x], (int)meta->last_clickL.axis[y], X,Y, (int)(meta->last_clickL.axis[x] - X)/4, (int)(meta->last_clickL.axis[y] - X)/4);
 		meta->map.ang[x] += ((int)meta->keys.last_clickL.axis[y] - Y);
 		meta->map.ang[y] += ((int)meta->keys.last_clickL.axis[x] - X);
 		if (meta->map.ang[x] < 0)
 			meta->map.ang[x] = 360 + meta->map.ang[x];
 		if (meta->map.ang[x] > 360)
-			meta->map.ang[x] = meta->map.ang[x] - 360;	
+			meta->map.ang[x] = meta->map.ang[x] - 360;
 		if (meta->map.ang[y] < 0)
 			meta->map.ang[y] = 360 + meta->map.ang[y];
 		if (meta->map.ang[y] > 360)
-			meta->map.ang[y] = meta->map.ang[y] - 360;	
+			meta->map.ang[y] = meta->map.ang[y] - 360;
 		meta->keys.last_clickL.axis[x] = X;
-		meta->keys.last_clickL.axis[y] = Y;			
+		meta->keys.last_clickL.axis[y] = Y;
 		draw_map(meta);
 	}
 	if (meta->keys.b_mouseR)
 	{
-		//printf("LastClickR [%d][%d] - Currect Click [%d][%d] - Increment [%d][%d]\n", (int)meta->keys.last_clickR.axis[x], (int)meta->keys.last_clickR.axis[y], X,Y, (int)(meta->keys.last_clickR.axis[x] - X)/50, (int)(meta->keys.last_clickR.axis[y] - X)/50);	
 		meta->map.source.axis[x] -= ((int)meta->keys.last_clickR.axis[x] - X);
 		meta->map.source.axis[y] -= ((int)meta->keys.last_clickR.axis[y] - Y);
 		meta->map.source.axis[z] = 0;
@@ -47,7 +43,7 @@ int	mouse_move(int X, int Y, void *param)
 		meta->keys.last_clickR.axis[y] = Y;
 		draw_map(meta);
 	}
-	return(0);
+	return (0);
 }
 
 int	mouse_release(int button, int X, int Y, void *param)
@@ -60,8 +56,8 @@ int	mouse_release(int button, int X, int Y, void *param)
 	if (button == 1)
 		meta->keys.b_mouseL = 0;
 	if (button == 2)
-		meta->keys.b_mouseR = 0;		
-	return(0);
+		meta->keys.b_mouseR = 0;
+	return (0);
 }
 
 int	mouse_press(int button, int X, int Y, void *param)
@@ -69,7 +65,6 @@ int	mouse_press(int button, int X, int Y, void *param)
 	t_meta	*meta;
 
 	meta = (t_meta *)param;
-
 	if (button == 1)
 	{
 		meta->keys.b_mouseL = 1;
@@ -84,17 +79,14 @@ int	mouse_press(int button, int X, int Y, void *param)
 	}	
 	if (button == 4)
 	{
-		// Zoom IN
 		meta->map.scale = meta->map.scale * 1.1;
 		draw_map(meta);
 	}
 	if (button == 5)
 	{
-		// Zoom OUT
 		if (meta->map.scale > 1)
 			meta->map.scale = meta->map.scale / 1.1;
 		draw_map(meta);
 	}
 	return (0);
-
 }
