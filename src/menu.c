c0a9ef9a2fb6854a125d6b03ee73dd0a8cb0c7dd
ailopez- <aitorlpzaudikana@gmail.com>
2022-07-18 23:22:24 +0200

    menu redesign

--- src/menu.c
+++ src/menu.c
@@ -13,10 +13,10 @@
 #include "../inc/utils.h"
 #include <stdlib.h>
 
-#define CONTROL_BOX			750
+#define CONTROL_BOX			650
 #define DRAWINFO_BOX		40
-#define MAPINFO_BOX			340
-#define COLORSCHEME_BOX		550
+#define MAPINFO_BOX			280
+#define COLORSCHEME_BOX		480
 #define LINE_SIZE			30			
 #define MENU_TAB			30
 
